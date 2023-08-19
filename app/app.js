f8698615da76b1d76c81c87fb032fd6a92da8cdf
kang cahya <cahyadyazin@yahoo.com>
2023-08-20 04:19:53 +0700

    update admob

--- app/app.js
+++ app/app.js
@@ -1,6 +1,10 @@
-import { Application } from '@nativescript/core'
+import { Application } from "@nativescript/core";
+import { firebase } from "@nativescript/firebase-core";
+import { Admob } from "@nativescript/firebase-admob";
 
-Application.run({ moduleName: 'app-root' })
+Application.run({ moduleName: "app-root" });
+firebase().initializeApp();
+Admob.init();
 
 /*
 Do not place any code after the application has been started as it will not
