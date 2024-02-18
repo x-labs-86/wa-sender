4be1242a737dcd567509f3e5499d0e672fbe7d58
kang cahya <cahyadyazin@yahoo.com>
2024-02-18 21:46:53 +0700

    force light mode

--- app/app.js
+++ app/app.js
@@ -1,7 +1,9 @@
 import { Application } from "@nativescript/core";
+import { Theme } from "@nativescript/theme";
 import { firebase } from "@nativescript/firebase-core";
 import { Admob, InterstitialAd } from "@nativescript/firebase-admob";
 
+Theme.setMode(Theme.Light); // Theme.Dark Or Theme.Light
 await firebase().initializeApp();
 Admob.init();
 
